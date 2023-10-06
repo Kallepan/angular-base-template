d8629960fd3f67a2d510c4238d5555fdd35aeb25
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-10-06 11:35:28 +0000

    feat: implemented signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, OnInit, inject } from '@angular/core';
+import { Component, HostBinding, OnInit, effect, inject, signal } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
@@ -11,38 +11,44 @@ export class AppComponent implements OnInit {
   private _overlayContainer = inject(OverlayContainer);
 
   // theme
-  private _isDark = false;
-  @HostBinding('class')
-  get themeMode() {
-    return this._isDark ? 'theme-dark' : 'theme-light';
+  private _isDark = signal(false);
+  @HostBinding('class') get themeMode() {
+    return this._isDark() ? 'theme-dark' : 'theme-light';
   }
 
   get isDark() {
-    return this._isDark;
+    return this._isDark();
   }
 
   toggleTheme() {
-    this._isDark = !this._isDark;
-    if (this._isDark) {
-      this._overlayContainer.getContainerElement().classList.remove('theme-dark');
-      this._overlayContainer.getContainerElement().classList.add('theme-light');
-    } else {
-      this._overlayContainer.getContainerElement().classList.remove('theme-light');
-      this._overlayContainer.getContainerElement().classList.add('theme-dark');
-    }
+    this._isDark.set(!this._isDark());
   }
 
   // sidenav
-  private _isSidenavOpen = false;
+  private _isSidenavOpen = signal(false);
   get isSidenavOpen() {
-    return this._isSidenavOpen;
+    return this._isSidenavOpen();
   }
   setSidenavState(state: boolean) {
-    this._isSidenavOpen = state;
+    this._isSidenavOpen.set(state);
   }
 
   // lifecycle hooks
+  constructor() {
+    effect(() => {
+      const isDark = this._isDark();
+      if (isDark) {
+        this._overlayContainer.getContainerElement().classList.remove('theme-dark');
+        this._overlayContainer.getContainerElement().classList.add('theme-light');
+      } else {
+        this._overlayContainer.getContainerElement().classList.remove('theme-light');
+        this._overlayContainer.getContainerElement().classList.add('theme-dark');
+      }
+    });
+  }
+
   ngOnInit(): void {
+    
     console.log('ngOnInit');
   }
 }
