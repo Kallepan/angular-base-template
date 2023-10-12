2eef22afe586f3027603a3fd5470dcc5943fce89
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-10-12 07:19:34 +0000

    feat: defined routes in app-routing instead of constants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 
+// Add routes with labels to display in the menu
 const routes: Routes = [
-  { path: 'composition', loadComponent: () => import('./pages/composition/composition.component').then(m => m.CompositionComponent) },
-  { path: 'standalone', loadComponent: () => import('./pages/standalone/standalone.component').then(m => m.StandaloneComponent) },
-  { path: 'bridge', loadComponent: () => import('./pages/bridge/view.component').then(m => m.ViewComponent) },
+  { 
+    path: 'composition', 
+    loadComponent: () => import('./pages/composition/composition.component').then(m => m.CompositionComponent),
+    data: { label: 'Composition' }
+  },
+  { 
+    path: 'standalone', 
+    loadComponent: () => import('./pages/standalone/standalone.component').then(m => m.StandaloneComponent), 
+    data: { label: 'Standalone' } 
+  },
+  { 
+    path: 'bridge', 
+    loadComponent: () => import('./pages/bridge/view.component').then(m => m.ViewComponent), 
+    data: { label: 'Bridge' } 
+  },
   { path: '', component: HomeComponent },
   { path: 'home', redirectTo: '' },
   { path: '**', redirectTo: '' },
