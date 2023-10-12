00af48a9a385eb77aab8425137073cd0dfbe379c
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-10-12 06:59:31 +0000

    fix: css for widgets

--- src/app/pages/bridge/view.component.ts
+++ src/app/pages/bridge/view.component.ts
@@ -4,13 +4,23 @@ import { BridgeModule } from './bridge.module';
 @Component({
   selector: 'app-view',
   template: `
+  <div class="container">
     <app-widget-wrapper>
       <app-velocity-widget></app-velocity-widget>
     </app-widget-wrapper>
     <app-widget-wrapper>
         <app-weather-widget></app-weather-widget>
     </app-widget-wrapper>
+  </div>
   `,
+  styles: [`
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin: 1rem auto;
+    }
+    `],
   standalone: true,
   imports: [BridgeModule],
 })
