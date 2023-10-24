8215eaaade0de0aed76db1ba0cae4190401a1c52
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-10-24 07:23:25 +0000

    feat: tests for formgroup

--- src/app/modules/reusable-forms/components/address-group/address-group.component.spec.ts
+++ src/app/modules/reusable-forms/components/address-group/address-group.component.spec.ts
@@ -1,21 +1,69 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddressGroupComponent } from './address-group.component';
+import { ControlContainer, FormControl, FormGroup, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
 
 describe('AddressGroupComponent', () => {
   let component: AddressGroupComponent;
   let fixture: ComponentFixture<AddressGroupComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AddressGroupComponent]
+    const fg: FormGroup = new FormGroup({
+      'answer': new FormControl(''),
     });
+    const fgd: FormGroupDirective = new FormGroupDirective([], []);
+    fgd.form = fg;
+
+    TestBed.configureTestingModule({
+      imports: [AddressGroupComponent, ReactiveFormsModule],
+      providers: [
+        {
+          provide: ControlContainer,
+          useValue: fgd
+        }
+      ]
+    });
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(AddressGroupComponent);
+    
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.controlKey = "test";
+    component.label = "test";
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add a new form group control when initialized', () => {
+    // Arrange
+    const formGroup = new FormGroup({
+      street: new FormControl(''),
+      zipCode: new FormControl('')
+    });
+    spyOn(component.parentFormGroup, 'addControl');
+
+    // Act
+    component.ngOnInit();
+
+    // Assert
+    expect(component.parentFormGroup.addControl).toHaveBeenCalledWith(
+      component.controlKey,
+      jasmine.any(FormGroup)
+    );
+  });
+
+  it('should remove the form group control when destroyed', () => {
+    // Arrange
+    spyOn(component.parentFormGroup, 'removeControl');
+
+    // Act
+    component.ngOnDestroy();
+
+    // Assert
+    expect(component.parentFormGroup.removeControl).toHaveBeenCalledWith('adress');
+  });
+
 });
