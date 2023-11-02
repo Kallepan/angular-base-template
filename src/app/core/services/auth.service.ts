5e0cef823d265354421d3a6bc974617da55cc7bf
Kajeepan Kandeepan <kandeepan@labmed.de>
2023-11-02 09:48:55 +0100

    feat: stump auth guard

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  isLoggedIn(): boolean {
+    return false;
+  }
+
+  isFeatureEnabled(flagName: string): boolean {
+    return false;
+  }
+}
